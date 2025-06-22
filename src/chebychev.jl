be5b91fe119be2c29998f282fb0ddd92974959b0
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2025-06-22 23:27:34 +0200

    implement momens

--- src/chebychev.jl
+++ src/chebychev.jl
@@ -2,7 +2,7 @@ using SpecialFunctions
 using Polynomials
 
 struct Chebyshev <: ContinuousUnivariateDistribution
-    polynomial::ChebyshevT{Float64,:x}
+    polynomial::ChebyshevT{Float64, :x}
     integral::Float64
     a::Float64
     b::Float64
@@ -40,3 +40,30 @@ function Base.rand(rng::AbstractRNG, d::Chebyshev)
     return (x * (d.b - d.a) + d.a + d.b) / 2
 end
 
+"""
+Compute n-th central moment of the distribution using numerical integration of the polynomial.
+It's implemented for [-1,1] range. See 
+https://github.com/JuliaHEP/DistributionsHEP.jl/issues/11
+"""
+function pure_moment(d::Chebyshev, n::Int; μ = mean(d))
+    pol_x_minus_mu = ChebyshevT([-μ, 1.0])
+    integrand = d.polynomial * pol_x_minus_mu^n
+    _int = integrate(integrand, -1.0, 1.0) / d.integral
+    return _int
+end
+
+Distributions.mean(model::Chebyshev) = pure_moment(model, 1, 0.0)
+
+Distributions.var(model::Chebyshev) = pure_moment(model, 2)
+
+function Distributions.skewness(model::Chebyshev)
+    σ = std(model)
+    μ3 = pure_moment(model, 3)
+    return μ3 / σ^3
+end
+
+function Distributions.kurtosis(model::Chebyshev)
+    σ = std(model)
+    μ4 = pure_moment(model, 4)
+    return μ4 / σ^4 - 3
+end
