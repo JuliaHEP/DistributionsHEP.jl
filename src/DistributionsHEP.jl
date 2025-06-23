a4a07501a1cf55ebe2452046dc64e29e1d58397c
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2025-06-23 09:17:31 +0200

    clean up import export

--- src/DistributionsHEP.jl
+++ src/DistributionsHEP.jl
@@ -1,12 +1,18 @@
 module DistributionsHEP
 
-using Random
 using Distributions
+using Distributions.Statistics
+using Distributions.StatsBase
 using SpecialFunctions
+using Random
 
 import Distributions: @check_args
-import Distributions: pdf, cdf, mean, var, skewness, kurtosis, entropy
-export pdf, cdf, mean, var, skewness, kurtosis, entropy
+import Distributions.Statistics: mean, std, var, quantile
+import Distributions.StatsBase: kurtosis, skewness
+import Base: maximum, minimum, rand
+
+export pdf, cdf, quantile, support
+export mean, std, var, skewness, kurtosis
 
 export Chebyshev
 include("chebychev.jl")
