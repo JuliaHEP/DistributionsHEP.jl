7b70b73216781fb1b37927e9fe092134d347f0fa
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2025-06-23 09:22:21 +0200

    add Linear transformation of chebychev

--- src/chebychev.jl
+++ src/chebychev.jl
@@ -2,16 +2,18 @@ using SpecialFunctions
 using Polynomials
 
 struct Chebyshev <: ContinuousUnivariateDistribution
-    polynomial::ChebyshevT{Float64,:x}
+    polynomial::ChebyshevT{Float64, :x}
     integral::Float64
     a::Float64
     b::Float64
-    function Chebyshev(coeffs, a = -1.0, b = 1.0)
+    function Chebyshev(coeffs)
         polynomial = ChebyshevT(coeffs)
         integral = integrate(polynomial)
-        new(polynomial, (integral(1.0) - integral(-1.0)), a, b)
+        new(polynomial, (integral(1.0) - integral(-1.0)), -1, 1)
     end
 end
+
+Chebyshev(coeffs, a::T, b::T) where {T <: Real} = Chebyshev(coeffs) * (b - a) / 2 + (a + b) / 2
 
 Distributions.maximum(d::Chebyshev) = d.b
 Distributions.minimum(d::Chebyshev) = d.a
